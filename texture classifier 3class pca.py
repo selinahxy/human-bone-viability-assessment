5e58702f1b4e8966ee978b6288eab3fc68d7bc07
selinahxy <61845772+selinahxy@users.noreply.github.com>
2021-11-22 10:09:50 -0500

    Update texture classifier 3class pca.py
    1. add patient foot-off information; 2. use customized cost functions; 3. delete svm classifier

--- texture classifier 3class pca.py
+++ texture classifier 3class pca.py
@@ -2,8 +2,7 @@ import pandas, numpy
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import GroupKFold, LeaveOneGroupOut, LeavePGroupsOut
-from sklearn import svm
+from sklearn.model_selection import GroupKFold
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.multiclass import OneVsOneClassifier
@@ -11,7 +10,7 @@ from sklearn.decomposition import PCA
 
 
 # import organized training dataset
-texture = pandas.read_csv('training 20features 3classes by 3d Clustering TTP-TTP+120s withtime offset2nl256 0328.csv')
+texture = pandas.read_csv('training footoff 20features 3classes 7patients round2 by DecisionOutlines TTP-TTP+120s withtime offset2nl256 1014.csv')
 array = texture.values
 # shuffle dataset
 numpy.random.shuffle(array)
@@ -22,35 +21,33 @@ scaler = MinMaxScaler().fit(x)
 rescaledX = scaler.transform(x)
 # select response variables
 y = array[:, 23]
-y = numpy.where(y == 2, 0, y)
-y = numpy.where(y == 3, 2, y)
 # select grouping variables
 groups_patient = array[:, 21]
-gkf = GroupKFold(n_splits=3)
+gkf = GroupKFold(n_splits=7)
 
 log_validation_acc = []
 log_test_acc = []
-log_sensitivity_de = []
-log_sensitivity_no = []
-svm_validation_acc = []
-svm_test_acc = []
-svm_sensitivity_de = []
-svm_sensitivity_no = []
+log_FNR = []
+log_FPR = []
+log_SB = []
 rf_validation_acc = []
 rf_test_acc = []
-rf_sensitivity_de = []
-rf_sensitivity_no = []
+rf_FNR = []
+rf_FPR = []
+rf_SB = []
 gb_validation_acc = []
 gb_test_acc = []
-gb_sensitivity_de = []
-gb_sensitivity_no = []
+gb_FNR = []
+gb_FPR = []
+gb_SB = []
 knn_validation_acc = []
 knn_test_acc = []
-knn_sensitivity_de = []
-knn_sensitivity_no = []
+knn_FNR = []
+knn_FPR = []
+knn_SB = []
 
 
-# grouped 3-fold cross validation
+# grouped 7-fold cross validation
 for train, test in gkf.split(rescaledX, y, groups=groups_patient):
     X_train, X_test, y_train, y_test = rescaledX[train], rescaledX[test], y[train], y[test]
     # principle component analysis
@@ -66,34 +63,12 @@ for train, test in gkf.split(rescaledX, y, groups=groups_patient):
     log_validation_acc.append(ovo_log.score(pc_train, y_train))
     log_test_acc.append(ovo_log.score(pc_test, y_test))
     cm1 = confusion_matrix(y_test, y_pred)
-    if numpy.array(cm1).size == 4:
-        cm11 = numpy.array([[cm1[0, 0], cm1[0, 1], 0],
-                        [cm1[1, 0], cm1[1, 1], 0],
-                        [0, 1, 0]])
-    else:
-        cm11 = cm1
-    sensitivity1 = cm11[1, 1] / (cm11[1, 0] + cm11[1, 1] + cm11[1, 2])
-    sensitivity12 = cm11[2, 2] / (cm11[2, 0] + cm11[2, 1] + cm11[2, 2])
-    log_sensitivity_de.append(sensitivity1)
-    log_sensitivity_no.append(sensitivity12)
-    # svm
-    svm_class = svm.SVC(kernel='rbf')
-    ovo_svm = OneVsOneClassifier(svm_class)
-    ovo_svm.fit(pc_train, y_train)
-    y_pred = ovo_svm.predict(pc_test)
-    svm_validation_acc.append(ovo_svm.score(pc_train, y_train))
-    svm_test_acc.append(ovo_svm.score(pc_test, y_test))
-    cm2 = confusion_matrix(y_test, y_pred)
-    if numpy.array(cm2).size == 4:
-        cm22 = numpy.array([[cm2[0, 0], cm2[0, 1], 0],
-                        [cm2[1, 0], cm2[1, 1], 0],
-                        [0, 1, 0]])
-    else:
-        cm22 = cm2
-    sensitivity2 = cm22[1, 1] / (cm22[1, 0] + cm22[1, 1] + cm22[1, 2])
-    sensitivity22 = cm22[2, 2] / (cm22[2, 0] + cm22[2, 1] + cm22[2, 2])
-    svm_sensitivity_de.append(sensitivity2)
-    svm_sensitivity_no.append(sensitivity22)
+    FNR1 = (cm1[1, 0] + cm1[2, 0])/(cm1[1, 0] + cm1[1, 1] + cm1[1, 2] + cm1[2, 0] + cm1[2, 1] + cm1[2, 2])  # cost function FNR
+    FPR1 = (cm1[0, 1] + cm1[0, 2])/(cm1[0, 0] + cm1[0, 1] + cm1[0, 2])  # cost function FPR
+    SB1 = (cm1[0, 1] + cm1[1, 1] + cm1[2, 1])/(cm1[1, 0] + cm1[1, 1] + cm1[1, 2])  # cost function SB
+    log_FNR.append(FNR1)
+    log_FPR.append(FPR1)
+    log_SB.append(SB1)
     # random forest classifier
     rf_class = RandomForestClassifier(n_estimators=100, random_state=0)
     rf_class.fit(pc_train, y_train)
@@ -101,16 +76,12 @@ for train, test in gkf.split(rescaledX, y, groups=groups_patient):
     rf_validation_acc.append(rf_class.score(pc_train, y_train))
     rf_test_acc.append(rf_class.score(pc_test, y_test))
     cm3 = confusion_matrix(y_test, y_pred)
-    if numpy.array(cm3).size == 4:
-        cm33 = numpy.array([[cm3[0, 0], cm3[0, 1], 0],
-                            [cm3[1, 0], cm3[1, 1], 0],
-                            [0, 1, 0]])
-    else:
-        cm33 = cm3
-    sensitivity3 = cm33[1, 1] / (cm33[1, 0] + cm33[1, 1] + cm33[1, 2])
-    sensitivity32 = cm33[2, 2] / (cm33[2, 0] + cm33[2, 1] + cm33[2, 2])
-    rf_sensitivity_de.append(sensitivity3)
-    rf_sensitivity_no.append(sensitivity32)
+    FNR3 = (cm3[1, 0] + cm3[2, 0]) / (cm3[1, 0] + cm3[1, 1] + cm3[1, 2] + cm3[2, 0] + cm3[2, 1] + cm3[2, 2])
+    FPR3 = (cm3[0, 1] + cm3[0, 2])/(cm3[0, 0] + cm3[0, 1] + cm3[0, 2])
+    SB3 = (cm3[0, 1] + cm3[1, 1] + cm3[2, 1])/(cm3[1, 0] + cm3[1, 1] + cm3[1, 2])
+    rf_FNR.append(FNR3)
+    rf_FPR.append(FPR3)
+    rf_SB.append(SB3)
     # gradient boosting
     gb_class = GradientBoostingClassifier(n_estimators=100, random_state=0)
     ovo_gb = OneVsOneClassifier(gb_class)
@@ -119,16 +90,12 @@ for train, test in gkf.split(rescaledX, y, groups=groups_patient):
     gb_validation_acc.append(ovo_gb.score(pc_train, y_train))
     gb_test_acc.append(ovo_gb.score(pc_test, y_test))
     cm4 = confusion_matrix(y_test, y_pred)
-    if numpy.array(cm4).size == 4:
-        cm43 = numpy.array([[cm4[0, 0], cm4[0, 1], 0],
-                            [cm4[1, 0], cm4[1, 1], 0],
-                            [0, 1, 0]])
-    else:
-        cm43 = cm4
-    sensitivity4 = cm43[1, 1] / (cm43[1, 0] + cm43[1, 1] + cm43[1, 2])
-    sensitivity42 = cm43[2, 2] / (cm43[2, 0] + cm43[2, 1] + cm43[2, 2])
-    gb_sensitivity_de.append(sensitivity4)
-    gb_sensitivity_no.append(sensitivity42)
+    FNR4 = (cm4[1, 0] + cm4[2, 0]) / (cm4[1, 0] + cm4[1, 1] + cm4[1, 2] + cm4[2, 0] + cm4[2, 1] + cm4[2, 2])
+    FPR4 = (cm4[0, 1] + cm4[0, 2])/(cm4[0, 0] + cm4[0, 1] + cm4[0, 2])
+    SB4 = (cm4[0, 1] + cm4[1, 1] + cm4[2, 1])/(cm4[1, 0] + cm4[1, 1] + cm4[1, 2])
+    gb_FNR.append(FNR4)
+    gb_FPR.append(FPR4)
+    gb_SB.append(SB4)
     # knn classifier
     knn_class = KNeighborsClassifier(n_neighbors=5)
     ovo_knn = OneVsOneClassifier(knn_class)
@@ -137,31 +104,25 @@ for train, test in gkf.split(rescaledX, y, groups=groups_patient):
     knn_validation_acc.append(ovo_knn.score(pc_train, y_train))
     knn_test_acc.append(ovo_knn.score(pc_test, y_test))
     cm5 = confusion_matrix(y_test, y_pred)
-    if numpy.array(cm5).size == 4:
-        cm55 = numpy.array([[cm5[0, 0], cm5[0, 1], 0],
-                        [cm5[1, 0], cm5[1, 1], 0],
-                        [0, 1, 0]])
-    else:
-     cm55 = cm5
-    sensitivity5 = cm55[1, 1] / (cm55[1, 0] + cm55[1, 1] + cm55[1, 2])
-    sensitivity52 = cm55[2, 2] / (cm55[2, 0] + cm55[2, 1] + cm55[2, 2])
-    knn_sensitivity_de.append(sensitivity5)
-    knn_sensitivity_no.append(sensitivity52)
+    FNR5 = (cm5[1, 0] + cm5[2, 0]) / (cm5[1, 0] + cm5[1, 1] + cm5[1, 2] + cm5[2, 0] + cm5[2, 1] + cm5[2, 2])
+    FPR5 = (cm5[0, 1] + cm5[0, 2])/(cm5[0, 0] + cm5[0, 1] + cm5[0, 2])
+    SB5 = (cm5[0, 1] + cm5[1, 1] + cm5[2, 1])/(cm5[1, 0] + cm5[1, 1] + cm5[1, 2])
+    knn_FNR.append(FNR5)
+    knn_FPR.append(FPR5)
+    knn_SB.append(SB5)
 
 
 # make and save resulting table
-result = numpy.array([[numpy.mean(log_validation_acc), numpy.mean(log_test_acc), numpy.mean(log_sensitivity_de), numpy.nanmean(log_sensitivity_no),
-                       numpy.std(log_test_acc), numpy.std(log_sensitivity_de), numpy.nanstd(log_sensitivity_no)],
-                      [numpy.mean(svm_validation_acc), numpy.mean(svm_test_acc), numpy.mean(svm_sensitivity_de), numpy.nanmean(svm_sensitivity_no),
-                       numpy.std(svm_test_acc), numpy.std(svm_sensitivity_de), numpy.nanstd(svm_sensitivity_no)],
-                      [numpy.mean(rf_validation_acc), numpy.mean(rf_test_acc), numpy.mean(rf_sensitivity_de), numpy.nanmean(rf_sensitivity_no),
-                       numpy.std(rf_test_acc), numpy.std(rf_sensitivity_de), numpy.nanstd(rf_sensitivity_no)],
-                      [numpy.mean(gb_validation_acc), numpy.mean(gb_test_acc), numpy.mean(gb_sensitivity_de), numpy.nanmean(gb_sensitivity_no),
-                       numpy.std(gb_test_acc), numpy.std(gb_sensitivity_de), numpy.nanstd(gb_sensitivity_no)],
-                      [numpy.mean(knn_validation_acc), numpy.mean(knn_test_acc), numpy.mean(knn_sensitivity_de), numpy.nanmean(knn_sensitivity_no),
-                       numpy.std(knn_test_acc), numpy.std(knn_sensitivity_de), numpy.nanstd(knn_sensitivity_no)]])
-resulttable = pandas.DataFrame(data=result, index=["log", "SVM", "random forest", "gradient boosting", "knn"],
-                               columns=["train accuracy", "validation accuracy", "sensitivity_semi", "sensitivity_non",
-                                        "std:validation accuracy", "std:sensitivity_semi", "std:sensitivity_non"])
-resulttable.to_csv (r'C:\Users\f00349n\Desktop\Python Scripts\20F3C fTTP-TTP120s 3dclustering pca nl256 2sensitivities add std.csv', header=True)
+result = numpy.array([[numpy.mean(log_validation_acc), numpy.mean(log_test_acc), numpy.mean(log_FNR), numpy.nanmean(log_FPR), numpy.nanmean(log_SB),
+                       numpy.std(log_test_acc), numpy.std(log_FNR), numpy.nanstd(log_FPR), numpy.nanmean(log_SB)],
+                      [numpy.mean(rf_validation_acc), numpy.mean(rf_test_acc), numpy.mean(rf_FNR), numpy.nanmean(rf_FPR), numpy.nanmean(rf_SB),
+                       numpy.std(rf_test_acc), numpy.std(rf_FNR), numpy.nanstd(rf_FPR), numpy.nanmean(rf_SB)],
+                      [numpy.mean(gb_validation_acc), numpy.mean(gb_test_acc), numpy.mean(gb_FNR), numpy.nanmean(gb_FPR), numpy.nanmean(gb_SB),
+                       numpy.std(gb_test_acc), numpy.std(gb_FNR), numpy.nanstd(gb_FPR), numpy.nanmean(gb_SB)],
+                      [numpy.mean(knn_validation_acc), numpy.mean(knn_test_acc), numpy.mean(knn_FNR), numpy.nanmean(knn_FPR), numpy.nanmean(knn_SB),
+                       numpy.std(knn_test_acc), numpy.std(knn_FNR), numpy.nanstd(knn_FPR), numpy.nanmean(knn_SB)]])
+resulttable = pandas.DataFrame(data=result, index=["log", "random forest", "gradient boosting", "knn"],
+                               columns=["train accuracy", "validation accuracy", "FNR", "FPR", "SB",
+                                        "std:validation accuracy", "std:sensitivity_semi", "std:sensitivity_non", "std:SB"])
+resulttable.to_csv (r'C:\Users\f00349n\Desktop\Python Scripts\20F3C fTTP-TTP120s decisionoutline 7patient footoff round2 pca nl256 corrected.csv', header=True)
 print(resulttable)
